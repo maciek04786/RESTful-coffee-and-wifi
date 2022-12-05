5653eeaa7c212f4d75dc2793672b0e75eaedacaa
maciek04786 <maciek_sikora_1986@gmail.com>
2022-12-05 12:47:52 +0000

    Footer edit, displays current year

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import os
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("8BYkEfBA6O6donzWlSihBXox7C0sKR6b")
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 Bootstrap(app)
 
 # CONNECT TO DB
